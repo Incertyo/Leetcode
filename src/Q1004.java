7c265c55b0d11fb09ab8c8e1b93a16bde74c1ab9
jagan <mohank0823@gmail.com>
2025-08-05 11:44:47 +0530

    5/08/25

---
+++ src/Q1004.java
@@ -0,0 +1,20 @@
+public class Q1004 {
+    class Solution {
+        public int longestOnes(int[] nums, int k) {
+            int l=0,r=0,zeroes=0,maxlen=0,len=0;
+            while(r<nums.length){
+                if(nums[r]==0)zeroes++;
+                if(zeroes>k){
+                    if(nums[l]==0)zeroes--;
+                    l++;
+                }
+                if(zeroes<=k){
+                    len= r-l+1;
+                    maxlen=Math.max(maxlen,len);
+                }
+                r++;
+            }
+            return maxlen;
+        }
+    }
+}
